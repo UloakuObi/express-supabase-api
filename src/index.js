28cc621727df3cb74b86e55718995ee17ddc21f5
UloakuObi <uloakuobi@yahoo.com>
2025-12-16 12:42:20 +0100

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Middleware
+app.use(express.json())
+app.use(cors())
+
+
+// Routes
+
+
+// Error Handlers
+
+
+// Server Running
+app.listen(port, () => {
+    console.log(`Listening on Port ${port}`)
+})
+
+
+// express
+// cors
+// dotenv
+// supabase
+// 
+
+// Create 
